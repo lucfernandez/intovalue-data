fe1e7fb3d1fd0e8d3f0325b3733ec5d57c6494ee
Luciano Fernandez <lucianofernandez2018@gmail.com>
2024-04-15 13:03:46 +0200

    Refactor to populate pubs_with_info with tidyverse

--- scripts/Publications_data.R
+++ scripts/Publications_data.R
@@ -22,59 +22,35 @@ pubs_supplementary_info <- trials %>%
 
 # pubs_with_crossreg will give us information about WHERE in the publication the cross-registered TRNs can be found
 pubs_from_crossreg <- cross_registrations %>%
-  filter(is_crossreg_secondary_id = TRUE | is_crossreg_abstract == TRUE | is_crossreg_ft == TRUE) %>% # filter out all cross-regs that aren't linked by a pub
+  filter(is_crossreg_secondary_id == TRUE | is_crossreg_abstract == TRUE | is_crossreg_ft == TRUE) %>% # filter out all cross-regs that aren't linked by a pub
   select(id, pmid, doi, crossreg_trn, crossreg_registry, is_crossreg_secondary_id, is_crossreg_abstract, is_crossreg_ft) %>%
   unique()
 
 ##############################################################################################
 
-# Merge two above tables to get publications with url and publication type info added
-pubs_with_info <- merge(pubs_from_crossreg, pubs_supplementary_info, by = "doi") %>%
-                 unique() %>%
-                 relocate(url, .after = pmid) %>%
-                 relocate(publication_type, .after = url) %>%
-                 rename(primary_IV_id = id) %>%
-                 relocate(primary_IV_id, .after = publication_type)
+pubs_with_info <-
+  left_join(pubs_from_crossreg, pubs_supplementary_info, by = "doi", relationship = "many-to-many") |>
+  relocate(url, publication_type, primary_IV_id = id, .after = pmid) |>
+  # `crossreg_trn` should never be NA
+  assertr::assert(assertr::not_na, crossreg_trn) |>
 
-# Once merged, can start putting TRNs in semicolon separated list in each column; initialize new columns
-pubs_with_info$trns_si <- NA
-pubs_with_info$trns_abs <- NA
-pubs_with_info$trns_ft <- NA
-pubs_with_info$trns_other <- NA
+  rename(
+    trns_si = is_crossreg_secondary_id,
+    trns_abs = is_crossreg_abstract,
+    trns_ft = is_crossreg_ft
+  ) |>
 
+  # Replace booleans with trn
+  mutate(across(starts_with("trns"), ~ if_else(. == TRUE, crossreg_trn, NA))) |>
 
-for (i in 1:nrow(pubs_with_info)) {
-
-  # If the crossreg_trn column in a row isn't blank AND is_crossreg_secondary is TRUE, then add the crossreg_trn in semicolon-separated
-  # list to trns_si
-  if (!is.na(pubs_with_info$crossreg_trn[i]) & pubs_with_info$is_crossreg_secondary_id[i]) {
-    pubs_with_info$trns_si[i] <- paste(pubs_with_info$crossreg_trn[i], pubs_with_info$trns_si[i], sep = ";")
-  }
-
-  # Same logic as above but with is_crossreg_abstract and trns_abs
-  if (!is.na(pubs_with_info$crossreg_trn[i]) & pubs_with_info$is_crossreg_abstract[i]) {
-    pubs_with_info$trns_abs[i] <- paste(pubs_with_info$crossreg_trn[i], pubs_with_info$trns_abs[i], sep = ";")
-  }
-
-  # is_crossreg_ft and trns_ft
-  if (!is.na(pubs_with_info$crossreg_trn[i]) & pubs_with_info$is_crossreg_ft[i]) {
-    pubs_with_info$trns_ft[i] <- paste(pubs_with_info$crossreg_trn[i], pubs_with_info$trns_ft[i], sep = ";")
-  }
-}
-
-# Drop unnecessary columns
-publications_clean <- pubs_with_info %>%
-                     select(-is_crossreg_secondary_id, -is_crossreg_abstract, -is_crossreg_ft)
-
-# Now we have multiple rows per publication, so lets collapse the table into one row per publication while conserving
-# all the information
-
-publications_final <- publications_clean %>%
-  group_by(pmid, doi, url, primary_IV_id) %>%
+  # Summarize to row per iv trn with all crossreg by location
+  group_by(pmid, doi, url, primary_IV_id) |>
   summarize(trns_si = paste(trns_si, collapse = ";"),
             trns_abs = paste(trns_abs, collapse = ";"),
             trns_ft = paste(trns_ft, collapse = ";"),
             url = paste(url, collapse = ";"),
-  )
+            .groups = "drop") |>
+  mutate(across(starts_with("trn"), ~ na_if(., "NA"))) |>
+  add_column(trns_other = NA)
 
-saveRDS(publications_final, "publications_final.rds")
+saveRDS(pubs_with_info, "publications_final.rds")
