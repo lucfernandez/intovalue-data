97e6001907f11f59dd058dd8b073b2fe8de70f01
Luciano Fernandez <lucianofernandez2018@gmail.com>
2024-05-25 13:59:18 +0200

    Handle random NAs in semicolon lists

--- scripts/Publications_data.R
+++ scripts/Publications_data.R
@@ -12,13 +12,7 @@ library(stringr)
 
 dir_processed <- here("data", "processed")
 
-trials <- read_csv(path(dir_processed, "trials.csv"))
 cross_registrations <- read_rds(path(dir_processed, "trn", "cross-registrations.rds"))
-
-# 'trials' gives us more information about URL and pub_type to supplement what we know from 'cross_registration'
-pubs_supplementary_info <- trials %>%
-                           select(doi, url, publication_type) %>%
-                           unique()
 
 # pubs_with_crossreg will give us information about WHERE in the publication the cross-registered TRNs can be found
 pubs_from_crossreg <- cross_registrations %>%
@@ -28,29 +22,33 @@ pubs_from_crossreg <- cross_registrations %>%
 
 ##############################################################################################
 
-pubs_with_info <-
-  left_join(pubs_from_crossreg, pubs_supplementary_info, by = "doi", relationship = "many-to-many") |>
-  relocate(url, publication_type, primary_IV_id = id, .after = pmid) |>
-  # `crossreg_trn` should never be NA
-  assertr::assert(assertr::not_na, crossreg_trn) |>
+pubs_with_info <- pubs_from_crossreg|>
+  rename(trns_si = is_crossreg_secondary_id,
+         trns_abs = is_crossreg_abstract,
+         trns_ft = is_crossreg_ft,
+         primary_IV_id = id) |>
 
-  rename(
-    trns_si = is_crossreg_secondary_id,
-    trns_abs = is_crossreg_abstract,
-    trns_ft = is_crossreg_ft
-  ) |>
-
-  # Replace booleans with trn
+  # Replace booleans in trns_si, trns_abs, and trns_ft with the TRN value in crossreg_trn if the boolean is TRUE
   mutate(across(starts_with("trns"), ~ if_else(. == TRUE, crossreg_trn, NA))) |>
 
   # Summarize to row per iv trn with all crossreg by location
-  group_by(pmid, doi, url, primary_IV_id) |>
+  group_by(pmid, doi, primary_IV_id) |>
   summarize(trns_si = paste(trns_si, collapse = ";"),
             trns_abs = paste(trns_abs, collapse = ";"),
             trns_ft = paste(trns_ft, collapse = ";"),
-            url = paste(url, collapse = ";"),
             .groups = "drop") |>
   mutate(across(starts_with("trn"), ~ na_if(., "NA"))) |>
+
+  # Eliminate all random and intrusive NAs in trns_si, trns_abs, and trns_ft
+  mutate(across(starts_with("trns"), ~ {
+
+    cleaned <- str_split(., ";") |> # split semicolon-separated strings into lists
+      lapply(function(x) x[x != "NA"]) |> # remove elements of lists that are equal to "NA" character
+      sapply(function(x) paste(x, collapse = ";")) # collapse elements back into semicolon-separated strings
+      ifelse(cleaned == "" | cleaned == "NA", NA, cleaned) # if result is an empty string or is still "NA", replace with NA value
+  })) |>
+
+   # Add final trns_other column for miscellaneous TRNs
   add_column(trns_other = NA)
 
 saveRDS(pubs_with_info, "data/publications_final.rds")
