1fbfc7b9df67478d68c315190461c96f92371205
Luciano Fernandez <lucianofernandez2018@gmail.com>
2024-07-17 16:06:33 +0200

    Condense long trn_trn table to single rows to ease manual validation

---
+++ scripts/trn_condensed.R
@@ -0,0 +1,148 @@
+# Script to condense duplicate entries in trn_trn table to make manual reconciliation easier.
+
+library(here)
+library(fs)
+library(lubridate)
+library(tidyverse)
+library(ctregistries)
+
+# Load long trn_trn table
+trn_trn <- read_rds("data/cross-registrations/trn_trn.rds")
+
+# Drop unnecessary columns, add new publication booleans
+trn_trn_no_regs <- trn_trn |>
+  select(-registry1, -registry2)
+
+# Create a unique identifier for pairs, which allows us to identify duplicate pairs
+trn_trn_pair_id <- trn_trn_no_regs %>%
+  rowwise |>
+  mutate(pair_id = paste(sort(c(trn1, trn2)), collapse = "_")) |>
+  ungroup()
+
+# Group and merge publication and trn1inreg2/trn2inreg1 information
+trn_merged_pubs <- trn_trn_pair_id %>%
+  group_by(pair_id) %>%
+
+  # Copy the trn2 boolean information of the other trial with the same pair_id into the other trials trn1 boolean fields
+  mutate(
+    trn1_in_pub_si = if_else(trn1 == first(trn1), lead(trn2_in_pub_si), lag(trn2_in_pub_si)),
+    trn1_in_pub_abs = if_else(trn1 == first(trn1), lead(trn2_in_pub_abs), lag(trn2_in_pub_abs)),
+    trn1_in_pub_ft = if_else(trn1 == first(trn1), lead(trn2_in_pub_ft), lag(trn2_in_pub_ft)),
+
+    trn1inreg2 = case_when(
+      trn1 == first(trn1) & (is.na(trn1inreg2) & is.na(lead(trn2inreg1))) ~ NA,
+      trn1 == first(trn1) & (trn1inreg2 == lead(trn2inreg1)) ~ trn1inreg2,
+      trn1 == first(trn1) & (is.na(trn1inreg2) | is.na(lead(trn2inreg1))) ~ coalesce(trn1inreg2, lead(trn2inreg1), FALSE),
+
+      trn1 != first(trn1) & (is.na(trn1inreg2) & is.na(lag(trn2inreg1))) ~ NA,
+      trn1 != first(trn1) & (trn1inreg2 == lag(trn2inreg1)) ~ trn1inreg2,
+      trn1 != first(trn1) & (is.na(trn1inreg2) | is.na(lag(trn2inreg1))) ~ coalesce(trn1inreg2, lag(trn2inreg1), FALSE),
+
+      .default = NA
+    ),
+    trn2inreg1 = case_when(
+      trn1 == first(trn1) & (is.na(trn2inreg1) & is.na(lead(trn1inreg2))) ~ NA,
+      trn1 == first(trn1) & (trn2inreg1 == lead(trn1inreg2)) ~ trn2inreg1,
+      trn1 == first(trn1) & (is.na(trn2inreg1) | is.na(lead(trn1inreg2))) ~ coalesce(trn2inreg1, lead(trn1inreg2), FALSE),
+
+      trn1 != first(trn1) & (is.na(trn2inreg1) & is.na(lag(trn1inreg2))) ~ NA,
+      trn1 != first(trn1) & (trn2inreg1 == lag(trn1inreg2)) ~ trn2inreg1,
+      trn1 != first(trn1) & (is.na(trn2inreg1) | is.na(lag(trn1inreg2))) ~ coalesce(trn2inreg1, lag(trn1inreg2), FALSE),
+
+      .default = NA
+    )
+  ) %>%
+  ungroup()
+
+# Merge protocol ID and title matching information
+trn_protocols_merged <- trn_merged_pubs |>
+  group_by(pair_id) |>
+  mutate(
+    is_match_protocol_sponsor_protocol_id = case_when(
+
+      # If either value is NA, take the first non-NA value. Will be TRUE or FALSE
+      is.na(lag(is_match_protocol_sponsor_protocol_id)) | is.na(lead(is_match_protocol_sponsor_protocol_id)) ~ coalesce(lag(is_match_protocol_sponsor_protocol_id), lead(is_match_protocol_sponsor_protocol_id), FALSE),
+
+      # If either value is TRUE, take TRUE
+      lag(is_match_protocol_sponsor_protocol_id) | lead(is_match_protocol_sponsor_protocol_id) ~ TRUE,
+
+      # All other cases, there is no match, set to FALSE
+      .default = FALSE
+    ),
+    is_match_results_sponsor_protocol_id = case_when(
+
+      is.na(lag(is_match_results_sponsor_protocol_id)) | is.na(lead(is_match_results_sponsor_protocol_id)) ~ coalesce(lag(is_match_results_sponsor_protocol_id), lead(is_match_results_sponsor_protocol_id), FALSE),
+      lag(is_match_results_sponsor_protocol_id) | lead(is_match_results_sponsor_protocol_id) ~ TRUE,
+      .default = FALSE
+    ),
+    is_title_matched = case_when(
+      is.na(lag(is_title_matched)) | is.na(lead(is_title_matched)) ~ coalesce(lag(is_title_matched), lead(is_title_matched), FALSE),
+      lag(is_title_matched) | lead(is_title_matched) ~ TRUE,
+      .default = FALSE)) |>
+  ungroup()
+
+# Merge meta booleans
+trn_merged_meta <- trn_protocols_merged |>
+  group_by(pair_id) |>
+  mutate(
+    at_least_one_pub = case_when(
+      lag(at_least_one_pub) | lead(at_least_one_pub) ~ TRUE,
+      !lag(at_least_one_pub) & !lead(at_least_one_pub) ~ FALSE,
+      .default = FALSE),
+    at_least_one_sponsor_match = case_when(
+      lag(at_least_one_sponsor_match) | lead(at_least_one_sponsor_match) ~ TRUE,
+      !lag(at_least_one_sponsor_match) & !lead(at_least_one_sponsor_match) ~ FALSE,
+      .default = FALSE)) |>
+  ungroup()
+
+# Drop duplicated row. They should have the same information now, so just drop the first.
+trn_no_dupes <- trn_merged_meta |>
+  group_by(pair_id) |>
+  slice_head(n= 1) |>
+  ungroup()
+
+# Reassign priorities based on Delwen's proposal:
+trn_priorities <- trn_no_dupes |>
+  mutate(priority = NA) |>
+  mutate(priority = case_when(
+
+    # Priority 1
+    at_least_one_EU &
+      at_least_one_IV &
+      trn1inreg2 &
+      trn2inreg1 &
+      is.na(priority) ~ 1,
+
+    # Priority 2
+    at_least_one_EU &
+      at_least_one_IV &
+      (trn1inreg2 | trn2inreg1) &
+      is.na(priority) ~ 2,
+
+    # Priority 3
+    at_least_one_EU &
+      at_least_one_IV &
+      is_title_matched &
+      is.na(priority) ~ 3,
+
+
+    # Priority 4
+    at_least_one_EU &
+      at_least_one_IV &
+      at_least_one_pub &
+      is.na(priority) ~ 4,
+
+    # Priority 5
+    at_least_one_EU &
+      at_least_one_IV &
+      at_least_one_sponsor_match &
+      is.na(priority )~ 5,
+
+    .default = 6
+  ))
+
+# Rearrange columns to make manual checks easiest
+trn_manual_checks <- trn_priorities |>
+  relocate(trn1_in_pub_si, trn1_in_pub_abs, trn1_in_pub_ft, .before = trn2_in_pub_si) |>
+  select(-pair_id)
+
